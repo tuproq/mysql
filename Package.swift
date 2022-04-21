8f541bec1c67a93b0f7c7e08abce43ea3d63e66f
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-04-21 22:56:37 +0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,19 +1,32 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.5
 
 import PackageDescription
 
 let package = Package(
     name: "tuproq-mysql",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v12),
+        .tvOS(.v13),
+        .watchOS(.v6)
+    ],
     products: [
         .library(name: "MySQL", targets: ["MySQL"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.2.0"),
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.17.0"),
-        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.7.2")
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
+        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.0.0")
     ],
     targets: [
-        .target(name: "MySQL", dependencies: ["Logging", "NIO", "NIOSSL"]),
-        .testTarget(name: "MySQLTests", dependencies: ["MySQL"])
-    ]
+        .target(name: "MySQL", dependencies: [
+            .product(name: "Logging", package: "swift-log"),
+            .product(name: "NIO", package: "swift-nio"),
+            .product(name: "NIOSSL", package: "swift-nio-ssl")
+        ]),
+        .testTarget(name: "MySQLTests", dependencies: [
+            .target(name: "MySQL")
+        ])
+    ],
+    swiftLanguageVersions: [.v5]
 )
